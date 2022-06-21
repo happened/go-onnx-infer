7a208586d93143f11034e5caa7db6b8a416a92b3
kejun.sheng <kejun.sheng@cyclone-robotics.com>
2022-06-21 14:39:40 +0800

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/happened/go-onnx-infer
+
+go 1.17
---
+++ main.go
@@ -0,0 +1,7 @@
+package go_onnx_infer
+
+/*
+* @Author: kejun.sheng
+* @Email: kejun.sheng@cyclone-robotics.com
+* @DateTime: 2022.06.21 14:38:21
+ */
